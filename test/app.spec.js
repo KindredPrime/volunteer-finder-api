91e679acf7f0be6312c62169c3710dd38ebd698f
Ken Wright <kenneth.gordon.wright@gmail.com>
2020-11-14 21:06:09 -0500

    Set up temporary tests

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,9 +1,11 @@
 const app = require('../src/app');
 
 describe('App', () => {
-  it('GET / responds with 200 containing "Hello, boilerplate!"', () => {
+  it('GET /api/* responds with 200 containing JSON', () => {
     return supertest(app)
-      .get('/')
-      .expect(200, 'Hello, boilerplate!');
-  })
+      .get('/api/*')
+      .expect(200, {
+        ok: true
+      });
+  });
 });
