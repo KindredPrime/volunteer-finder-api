f1aa49a007bf0f40439aed99b9dd4bd935157674
Ken Wright <kenneth.gordon.wright@gmail.com>
2020-11-19 10:59:35 -0500

    Create seed script for causes

---
+++ seeds/seed.causes.sql
@@ -0,0 +1,15 @@
+TRUNCATE causes RESTART IDENTITY CASCADE;
+
+INSERT INTO causes (cause_name)
+  VALUES
+    ('Youth'),
+    ('Health'),
+    ('Disaster Response'),
+    ('Animals'),
+    ('History'),
+    ('Education'),
+    ('Human Rights'),
+    ('Arts'),
+    ('Wildlife and Environment'),
+    ('Elderly'),
+    ('Poverty');
