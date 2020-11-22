687e70d24fb49e0b9feeecf9fc8db8db0fea0a8c
Ken Wright <kenneth.gordon.wright@gmail.com>
2020-11-22 11:40:41 -0500

    Create GET /api/users/:id endpoint

--- src/users/users-router.js
+++ src/users/users-router.js
@@ -24,4 +24,25 @@ usersRouter
       .catch(next);
   });
 
+usersRouter
+  .route('/:id')
+  .all((req, res, next) => {
+    const { id } = req.params;
+    return UsersService.getById(req.app.get('db'), id)
+      .then((user) => {
+        if (!user) {
+          return res
+            .status(404)
+            .json({ message: `User with id ${id} does not exist` });
+        }
+
+        res.user = user;
+        next();
+      })
+      .catch(next);
+  })
+  .get((req, res, next) => {
+    return res.json(sanitizeUser(res.user));
+  });
+
 module.exports = usersRouter;
--- test/users-endpoints.spec.js
+++ test/users-endpoints.spec.js
@@ -2,7 +2,7 @@ const knex = require('knex');
 const app = require('../src/app');
 const { makeUsersArray, makeMaliciousUser } = require('./users-fixtures');
 
-describe.only('Users Endpoints', () => {
+describe('Users Endpoints', () => {
   let db;
   before('Connect to database', () => {
     db = knex({
@@ -59,6 +59,51 @@ describe.only('Users Endpoints', () => {
         return supertest(app)
           .get('/api/users')
           .expect(200, [sanitizedUser]);
+      });
+    });
+  });
+
+  describe.only('GET /api/users/:id', () => {
+    context('Given no users', () => {
+      it('Responds with 404 and an error message', () => {
+        const id = 1000;
+        return supertest(app)
+          .get(`/api/users/${id}`)
+          .expect(404, { message: `User with id ${id} does not exist` });
+      });
+    });
+
+    context('Given the table has users', () => {
+      const testUsers = makeUsersArray();
+
+      beforeEach('Populate users', () => {
+        return db
+          .insert(testUsers)
+          .into('users');
+      });
+
+      it('Responds with 200 and the user with id', () => {
+        const id = 1;
+        return supertest(app)
+          .get(`/api/users/${id}`)
+          .expect(200, testUsers[id - 1]);
+      });
+    });
+
+    context('Given the user has XSS attack content', () => {
+      const { maliciousUser, sanitizedUser } = makeMaliciousUser();
+
+      beforeEach('Populate users with malicious content', () => {
+        return db
+          .insert(maliciousUser)
+          .into('users');
+      });
+
+      it('Responds with 200 and the user with id, without its XSS content', () => {
+        const id = 1;
+        return supertest(app)
+          .get(`/api/users/${id}`)
+          .expect(200, sanitizedUser);
       });
     });
   });
