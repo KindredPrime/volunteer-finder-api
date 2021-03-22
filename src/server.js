823c77fb0b7c9406f7f0158c6706739fa97308ec
Ken Wright <kenneth.gordon.wright@gmail.com>
2021-03-22 19:29:02 -0400

    Require SSL for database connections

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 const app = require('./app');
-const { PORT, DATABASE_URL } = require('./config');
+const { PORT, DATABASE_URL, NODE_ENV } = require('./config');
 const knex = require('knex');
 const logger = require('./logger');
 
 const db = knex({
   client: 'pg',
-  connection: DATABASE_URL
+  connection: {
+    connectionString: DATABASE_URL,
+    sslmode: NODE_ENV === 'production' ? 'require' : 'disable'
+  }
 });
 
 app.set('db', db);
